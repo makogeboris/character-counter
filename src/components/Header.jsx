c3343ef715fc74e8fa8fd1f5e37a445bc0bf856f
makoge boris <makogeboris@gmail.com>
2025-05-04 09:10:00 +0100

    modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import ThemeToggle from "./ThemeToggle";
 function Header({ theme, toggleTheme }) {
   return (
     <header>
-      <div className="mx-auto flex w-full max-w-[61.875rem] flex-col items-center gap-14 px-4 pt-4 pb-10 sm:px-6 sm:pt-[1.125rem] md:gap-12 md:pt-8 md:pb-12">
+      <div className="mx-auto flex w-full max-w-[63.375rem] flex-col items-center gap-14 px-4 pt-4 pb-10 sm:px-6 sm:pt-[1.125rem] md:gap-12 md:pt-8 md:pb-12">
         <div className="flex w-full items-center justify-between">
           <Logo theme={theme} />
           <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -76,7 +76,7 @@ function MainContainer() {
 
   return (
     <main>
-      <div className="mx-auto mb-8 flex w-full max-w-[61.875rem] flex-col items-center gap-8 px-4 sm:mb-24 sm:px-6 md:mb-48">
+      <div className="mx-auto mb-8 flex w-full max-w-[63.375rem] flex-col items-center gap-8 px-4 sm:mb-24 sm:px-6 md:mb-48">
         <form className="flex w-full flex-col gap-3">
           <Textarea
             text={text}
